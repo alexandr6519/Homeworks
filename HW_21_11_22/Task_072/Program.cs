d6713d0e7b2e7b6c2aae018e9c86dcad7f0d7dc3
Ikonnikov Aleksandr <alexandr6519@mail.ru>
2022-11-23 14:28:48 +0700

    adding of files

---
+++ HW_21_11_22/Task_072/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Please, input the integer number!");
+int number = Convert.ToInt32(Console.ReadLine());
+int numberAbs = Math.Abs(number);
+
+
+if (numberAbs < 100)
+{
+    Console.WriteLine("Inputing number has no the theird digital!");
+}
+else {
+    int result = 0;
+    while (numberAbs > 999)
+    {
+        numberAbs = numberAbs / 10;
+        result = numberAbs  % 10;        
+    }
+Console.WriteLine($"Inputing number has the theird digital is {result}!");
+}
